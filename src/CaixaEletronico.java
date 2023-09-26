9384251775e2d841daba6526386c5189337666fe
Guilherme Henrique <ghn.cnc@gmail.com>
2023-09-26 09:46:02 -0300

    Controle de fluxo

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -2,7 +2,7 @@ public class CaixaEletronico {
     public static void main(String[] args){
 
         double saldo = 25.0;
-        double valorSolicitado = 23.0;
+        double valorSolicitado = 20.0;
 
         if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
